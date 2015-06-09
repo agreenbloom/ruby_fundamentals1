70807738d80c13414087690b9e16bf3b94137219
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-09 14:31:17 -0400

    Day two Assignment commit Exercise 2 and first step exercise 3

---
+++ exercise2.rb
@@ -0,0 +1,24 @@
+#part 1 calculating a 20% tip 
+
+meal = 55
+
+tip = meal * 0.20
+puts tip
+
+
+#part 2 converting integers to string
+string1 = "A good tips is 11 dollars"
+integer1 = 11 
+
+puts integer1.to_s
+
+#part 3 multiplication with string interpolation
+
+num1 = 45628
+num2 = 7839
+
+puts "num1 * num2 is #{num1 * num2}"
+
+#part 4 testing Booleans
+
+(true && false) || (false && true) || !(false && false)
