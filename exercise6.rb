977e8adcfe58be61ede2a50db63a738c631b7c13
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-10 15:04:40 -0400

    updated exercise 6 - grocery list method updated

--- exercise6.rb
+++ exercise6.rb
@@ -1,7 +1,21 @@
 grocery_list = ["carrots", "apples", "oranges", "bananas", "rice"]
 
-while grocery_list
-  puts *grocery_list
-  break
+# while grocery_list
+#   puts *grocery_list
+
+#   break
+# end
+
+
+def list(value)
+  value.each do |item|
+   puts "* #{item}"
+  end
 end
+
+
+list(grocery_list)
+
+
+puts grocery_list.size
 
