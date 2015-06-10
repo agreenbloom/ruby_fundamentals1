cd5b8d47c40986c3531f5592fe1e30c3ea33d677
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-10 16:34:26 -0400

    added cohort four to list

---
+++ exercise7.rb
@@ -0,0 +1,17 @@
+studentsHash = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+def student_displ(value1)
+  value1.each do |cohort, student|
+    puts "#{cohort} #{student}"
+  end
+end
+
+student_displ(studentsHash)
+
+studentsHash[:cohort4] = 43
+
+student_displ(studentsHash)
