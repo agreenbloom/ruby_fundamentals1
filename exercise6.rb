745233e55c61a15119d9ccfe6aa83912fb5bf195
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-06-10 14:18:22 -0400

    exercise6 updated grocery list with added ingredient

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,7 @@
-grocery_list = ["carrots", "apples", "oranges", "bananas"]
+grocery_list = ["carrots", "apples", "oranges", "bananas", "rice"]
 
 while grocery_list
   puts *grocery_list
   break
 end
+
